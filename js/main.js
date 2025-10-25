6d2e004f45e3f3cfa406c7e5fcf290939e8a26aa
makinosoo7062 <makinosoo5932@nifty.com>
2025-10-25 16:26:18 +0900

    WIP:

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@
 
 {
 
-
-
+  const name = document.getElementById('name');
+  const tel = document.getElementById('tel');
+  const mail = document.getElementById('mail');
+  const message = document.getElementById('message');
+  const btn = document.getElementById('btn');
 
   
+
+
 }
+
